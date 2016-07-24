f38b732f488203fd2240ba8d7c50fd5c0282fb09
Alex Coles <alex@alexbcoles.com>
2016-07-24 17:06:34 +0100

    Add an additional route to step2
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- step2/app.rb
+++ step2/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  # NOTE: this must be a Symbol, otherwise it is interpreted as a String
+  # NOTE: this must be a Symbol, otherwise it expects a String template
   erb :index, layout: :layout
 end
 
@@ -15,4 +15,8 @@ end
 # http://www.sinatrarb.com/faq.html#slash
 get '/about/?' do
   erb :about, layout: :layout
+end
+
+get '/welcome/:name/?' do
+  erb "<h1>Welcome to my site #{params[:name]}</h1>"
 end
